06933056ef2c394c05ce5573e1574b6f4d2505a9
Abdykaparov <nurislamabdyk@gmail.com>
2022-10-25 12:05:26 +0600

    init:start project

--- 21-10 Зад/script.js
+++ 21-10 Зад/script.js
@@ -3,3 +3,8 @@
 
 name2 = name1
  alert(name2)
+ alert('Привет! Нурислам');
+ alert('Ashimov');
+ let age = 18;
+ alert(` Мне ${age} лет`);
+ alert(`Салам Бишкек`);
---
+++ 24-10 home work/script.js
@@ -0,0 +1,5 @@
+alert('Привет! Нурислам');
+alert('Ashimov');
+let age = 18;
+alert(` Мне ${age} лет`);
+alert(`Салам Бишкек`);
