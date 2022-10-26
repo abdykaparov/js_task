5103fc449919e70471f2eda2a834ab4c508e8fe9
Abdykaparov <nurislamabdyk@gmail.com>
2022-10-26 10:33:33 +0600

    fead: add task

---
+++ 25-10 home work/script.js
@@ -0,0 +1,19 @@
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7
+let result = c + d;
+console.log(result);
+
+
+let hour = 11;
+let min = 45;
+let sec = 34;
+let time = `${hour}:${min}:${sec}`;
+console.log(time);
+
+
+let num = 97;
+num = num++;
+// num = ++num;
+alert(num);
