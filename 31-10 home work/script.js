528e9f2145b3a81cd64c449262197f879f6c0700
Abdykaparov <nurislamabdyk@gmail.com>
2022-11-09 08:45:11 +0600

    feat:add task

---
+++ 31-10 home work/script.js
@@ -0,0 +1,14 @@
+/********** 1 **********/
+let userSalaries = {
+    Ella: 17,
+    Sophia: 25,
+    Ellie: 34,
+};
+console.log('zarplata Ella: ${userSalaries, Ella}, Sophia: ${userSalaries, Sophia }' );
+
+/********** 2 **********/
+let key = {
+    a: 2,
+    b: 4,
+    c: 6,
+}
