f2c89c44dadd86816701f944c61b599c65320870
Abdykaparov <nurislamabdyk@gmail.com>
2022-11-10 09:12:01 +0600

    feat:add task

---
+++ 09-11 home wokr/script.js
@@ -0,0 +1,18 @@
+// const hello = (massage) => {
+//     alert(massage);
+//
+// }
+//
+// const timeId = setTimeout(hello, 4000, 'hello Mara');
+// setTimeout(hello, 8000, 'Nurs');
+// setTimeout(timeId);
+
+
+
+/********** 2 **********/
+const YearOfBirth = +prompt(' год рождения');
+const ThisYear = +prompt('cегодняший год');
+
+result = YearOfBirth - ThisYear;
+
+alert(`вам ${result} лет`);
